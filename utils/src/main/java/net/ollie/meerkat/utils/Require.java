63868eee762cca7014abc3b5e7c3150a67832c66
Ollie <ollie.robertshaw@gmail.com>
2016-03-31 08:23:23 +0100

    Require

--- utils/src/main/java/net/ollie/meerkat/utils/Require.java
+++ utils/src/main/java/net/ollie/meerkat/utils/Require.java
@@ -12,6 +12,20 @@ public class Require {
     protected Require() {
     }
 
+    public static void positive(final int value) {
+        positive(value, () -> "Value was not positive: " + value);
+    }
+
+    public static void positive(final int value, final Supplier<String> message) {
+        if (value <= 0) {
+            throw new IllegalArgumentException(message.get());
+        }
+    }
+
+    public static void argumentsEqual(final Object left, final Object right) {
+        argumentsEqual(left, right, () -> "Objects were not equal: [" + left + "} != [" + right + "]");
+    }
+
     public static void argumentsEqual(final Object left, final Object right, final Supplier<String> message) {
         if (!Objects.equals(left, right)) {
             throw new IllegalArgumentException(message.get());
