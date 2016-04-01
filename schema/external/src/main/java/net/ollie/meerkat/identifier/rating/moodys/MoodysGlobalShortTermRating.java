3863ad383720492a0650997c5dc44ffdc17eb268
Ollie Robertshaw <ollie.robertshaw@gmail.com>
2016-04-01 08:31:11 +0100

    Ratings

--- schema/external/src/main/java/net/ollie/meerkat/identifier/rating/moodys/MoodysGlobalShortTermRating.java
+++ schema/external/src/main/java/net/ollie/meerkat/identifier/rating/moodys/MoodysGlobalShortTermRating.java
@@ -1,8 +1,5 @@
 package net.ollie.meerkat.identifier.rating.moodys;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.Set;
 
 import javax.xml.bind.annotation.XmlEnum;
@@ -10,6 +7,7 @@ import javax.xml.bind.annotation.XmlEnum;
 import net.ollie.meerkat.issue.rating.CreditRating;
 import net.ollie.meerkat.issue.rating.CreditRatingBand;
 import static net.ollie.meerkat.issue.rating.CreditRatingBand.*;
+import net.ollie.meerkat.utils.collections.Sets;
 
 /**
  *
@@ -20,15 +18,16 @@ public enum MoodysGlobalShortTermRating implements MoodysRating {
 
     P1("Superior ability to repay short-term debt obligations", AAA, AA, A),
     P2("Strong ability to repay short-term debt obligations", A, BBB),
-    P3("Acceptable abiluty to repay short-term debt obligations", BBB),
-    NP("Not prime", BB, B, CCC, CC, C, D, U);
+    P3("Acceptable ability to repay short-term debt obligations", BBB),
+    NP("Not prime", BB, B, CCC, CC, C),
+    NR("Not rated", U);
 
     private final String description;
     private final Set<CreditRatingBand> bands;
 
     private MoodysGlobalShortTermRating(final String description, final CreditRatingBand... bands) {
         this.description = description;
-        this.bands = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(bands)));
+        this.bands = Sets.asUnmodifiableSet(bands);
     }
 
     @Override
---
+++ schema/external/src/main/java/net/ollie/meerkat/identifier/rating/moodys/MoodysNationalShortTermRating.java
@@ -0,0 +1,38 @@
+package net.ollie.meerkat.identifier.rating.moodys;
+
+import java.util.Set;
+
+import net.ollie.meerkat.issue.rating.CreditRatingBand;
+import static net.ollie.meerkat.issue.rating.CreditRatingBand.*;
+import net.ollie.meerkat.utils.collections.Sets;
+
+/**
+ *
+ * @author ollie
+ */
+public enum MoodysNationalShortTermRating implements MoodysRating {
+
+    N1("Strong ability to repay short-term debt", AAA, AA, A),
+    N2("Above average ability to repay short-term debt", BBB),
+    N3("Average ability to repay short-term debt", BB),
+    N4("Below average ability to repay short-term debt", B);
+
+    private final String description;
+    private final Set<CreditRatingBand> bands;
+
+    private MoodysNationalShortTermRating(final String description, final CreditRatingBand... bands) {
+        this.description = description;
+        this.bands = Sets.asUnmodifiableSet(bands);
+    }
+
+    @Override
+    public String description() {
+        return description;
+    }
+
+    @Override
+    public Set<CreditRatingBand> bands() {
+        return bands;
+    }
+
+}
