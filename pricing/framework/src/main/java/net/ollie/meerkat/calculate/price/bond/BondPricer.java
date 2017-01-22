1db2a92840059b87c21f7cd3e6d9762f34ff10e9
Ollie <ollie.robertshaw@gmail.com>
2017-01-22 14:31:34 +0000

    Tighten interface

--- pricing/framework/src/main/java/net/ollie/meerkat/calculate/price/bond/BondPricer.java
+++ pricing/framework/src/main/java/net/ollie/meerkat/calculate/price/bond/BondPricer.java
@@ -2,28 +2,30 @@ package net.ollie.meerkat.calculate.price.bond;
 
 import java.time.temporal.Temporal;
 
-import javax.annotation.Nonnull;
-
 import net.meerkat.money.currency.Currency;
-import net.ollie.meerkat.calculate.price.SecurityPriceException;
+import net.ollie.meerkat.calculate.price.SecurityTypePriceCalculator;
 import net.meerkat.security.bond.Bond;
+import net.ollie.meerkat.calculate.price.SecurityPriceException;
 
 /**
+ * Prices particular types of bond.
  *
- * @author Ollie
+ * @author ollie
+ * @see GenericBondPricer
  */
-public interface BondPricer<T extends Temporal> extends BondTypePricer<T, Bond> {
+public interface BondPricer<T extends Temporal, B extends Bond>
+        extends SecurityTypePriceCalculator<T, B> {
 
     @Override
-    default <C extends Currency> BondPrice.Shiftable<C> price(
-            final T valuation,
-            final Bond bond,
-            final C currency)
-            throws BondPriceException {
-        return bond.handleWith(this.priceContext(valuation, currency));
+    default <C extends Currency> BondPrice.Shiftable<C> price(T temporal, B bond, C currency) throws BondPriceException {
+        return bond.handleWith(this.priceContext(temporal, currency));
     }
 
-    <C extends Currency> BondPriceContext<C> priceContext(T valuation, C currency)
+    default <C extends Currency> BondPrice.Shiftable<C> price(final T temporal, final B bond, final C currency, final BondShifts shifts) throws BondPriceException {
+        return this.price(temporal, bond, currency).shift(shifts);
+    }
+
+    <C extends Currency> GenericBondPricer.BondPriceContext<C> priceContext(T valuation, C currency)
             throws BondPriceException;
 
     interface BondPriceContext<C extends Currency> extends Bond.Handler<BondPrice.Shiftable<C>> {
---
+++ pricing/framework/src/main/java/net/ollie/meerkat/calculate/price/bond/GenericBondPricer.java
@@ -0,0 +1,24 @@
+package net.ollie.meerkat.calculate.price.bond;
+
+import java.time.temporal.Temporal;
+
+import net.meerkat.money.currency.Currency;
+import net.meerkat.security.bond.Bond;
+
+/**
+ * Tries to price any type of bond by considering the coupon payments.
+ *
+ * @author Ollie
+ */
+public interface GenericBondPricer<T extends Temporal> extends BondPricer<T, Bond> {
+
+    @Override
+    default <C extends Currency> BondPrice.Shiftable<C> price(
+            final T temporal,
+            final Bond bond,
+            final C currency)
+            throws BondPriceException {
+        return bond.handleWith(this.priceContext(temporal, currency));
+    }
+
+}
