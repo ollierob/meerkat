6c3beba3821eb192ff82786ed0687292b67d7f04
Ollie Robertshaw <ollie.robertshaw@gmail.com>
2017-01-24 20:41:15 +0000

    Explanation

--- schema/security/src/main/java/net/meerkat/instrument/interest/option/InterestRateOption.java
+++ schema/security/src/main/java/net/meerkat/instrument/interest/option/InterestRateOption.java
@@ -2,9 +2,9 @@ package net.meerkat.instrument.interest.option;
 
 import javax.xml.bind.annotation.XmlElementRef;
 
-import net.meerkat.numeric.interest.InterestRateSecurity;
 import net.meerkat.instrument.derivative.option.AbstractOption;
 import net.meerkat.instrument.interest.InterestRateDerivative;
+import net.meerkat.numeric.interest.InterestRateSecurity;
 
 /**
  *
@@ -13,6 +13,8 @@ import net.meerkat.instrument.interest.InterestRateDerivative;
 public class InterestRateOption
         extends AbstractOption<InterestRateSecurity>
         implements InterestRateDerivative {
+
+    private static final long serialVersionUID = 1L;
 
     @XmlElementRef(name = "underlying")
     private InterestRateSecurity underlying;
@@ -29,6 +31,12 @@ public class InterestRateOption
     @Override
     public <R> R handleWith(final InterestRateDerivative.Handler<R> handler) {
         return handler.handle(this);
+    }
+
+    @Override
+    public ExplanationBuilder explain() {
+        return super.explain()
+                .put("underlying", underlying);
     }
 
 }
--- schema/security/src/main/java/net/meerkat/instrument/interest/option/Swaption.java
+++ schema/security/src/main/java/net/meerkat/instrument/interest/option/Swaption.java
@@ -2,9 +2,9 @@ package net.meerkat.instrument.interest.option;
 
 import javax.xml.bind.annotation.XmlElementRef;
 
+import net.meerkat.instrument.derivative.option.AbstractOption;
 import net.meerkat.instrument.interest.InterestRateDerivative;
 import net.meerkat.instrument.interest.swap.InterestRateSwap;
-import net.meerkat.instrument.derivative.option.AbstractOption;
 
 /**
  *
@@ -13,6 +13,8 @@ import net.meerkat.instrument.derivative.option.AbstractOption;
 public class Swaption
         extends AbstractOption<InterestRateSwap>
         implements InterestRateDerivative {
+
+    private static final long serialVersionUID = 1L;
 
     @XmlElementRef(name = "underlying")
     private InterestRateSwap underlying;
@@ -29,6 +31,12 @@ public class Swaption
     @Override
     public <R> R handleWith(final InterestRateDerivative.Handler<R> handler) {
         return handler.handle(this);
+    }
+
+    @Override
+    public ExplanationBuilder explain() {
+        return super.explain()
+                .put("underlying", underlying);
     }
 
 }
