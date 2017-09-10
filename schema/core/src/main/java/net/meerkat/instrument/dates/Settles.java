a8feb9ea87a50d14cbcb7818e3795391e9022f0f
Ollie <ollie.robertshaw@gmail.com>
2017-09-10 11:42:50 +0100

    Dates

--- schema/core/src/main/java/net/meerkat/instrument/dates/Settles.java
+++ schema/core/src/main/java/net/meerkat/instrument/dates/Settles.java
@@ -1,18 +1,24 @@
 package net.meerkat.instrument.dates;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 import javax.annotation.Nonnull;
 
 /**
  * @author ollie
  */
-public interface Settles {
+public interface Settles extends Traded {
 
     /**
      * @return the date on which the transfer of cash or assets occurs.
      */
     @Nonnull
     LocalDate settlementDate();
+
+    @Nonnull
+    default Period tradeToSettlement() {
+        return Period.between(this.tradeDate(), this.settlementDate());
+    }
 
 }
