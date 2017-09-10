90972536f9f64c0ae5418b4d740146787b64073c
Ollie <ollie.robertshaw@gmail.com>
2017-09-10 15:46:08 +0100

    Book

---
+++ risk/framework/src/main/java/net/meerkat/risk/portfolio/Book.java
@@ -0,0 +1,34 @@
+package net.meerkat.risk.portfolio;
+
+import net.coljate.map.Map;
+import net.coljate.set.Set;
+import net.meerkat.identifier.position.PositionId;
+import net.meerkat.risk.position.Position;
+import net.meerkat.risk.position.PositionProvider;
+
+/**
+ * Portfolio of positions with no children.
+ *
+ * @author Ollie
+ */
+public interface Book extends Portfolio {
+
+    @Override
+    @Deprecated
+    default Set<PortfolioId> childPortfolioIds() {
+        return Set.of();
+    }
+
+    @Override
+    @Deprecated
+    default Map<PortfolioId, ? extends Book> childPortfolios(final PortfolioProvider portfolios) {
+        return Map.of();
+    }
+
+    @Override
+    @Deprecated
+    default Map<PositionId, ? extends Position> childPositions(PortfolioProvider portfolioProvider, PositionProvider positionProvider) {
+        return Map.of();
+    }
+
+}
