e440dd947361d097effaf06918de7ea549b91800
Ollie Robertshaw <ollie.robertshaw@gmail.com>
2017-09-23 20:25:23 +0100

    Test

--- schema/core/src/test/java/net/meerkat/time/calendar/settlement/SettlementDatePairCalendarTest.java
+++ schema/core/src/test/java/net/meerkat/time/calendar/settlement/SettlementDatePairCalendarTest.java
@@ -1,6 +1,13 @@
 package net.meerkat.time.calendar.settlement;
 
+import java.time.LocalDate;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import org.junit.jupiter.api.Test;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  *
@@ -10,7 +17,35 @@ import org.junit.jupiter.api.Test;
 public class SettlementDatePairCalendarTest {
 
     @Test
-    public void testSomeMethod() {
+    public void testArabSettlement() {
+
+        final SettlementDateCalendar usSettlements = mock(SettlementDateCalendar.class);
+        when(usSettlements.isInRange(any(LocalDate.class))).thenReturn(true);
+
+        final SettlementDateCalendar arabSettlements = mock(SettlementDateCalendar.class);
+        when(arabSettlements.isInRange(any(LocalDate.class))).thenReturn(true);
+
+        final SettlementDatePairCalendar pair = new SettlementDatePairCalendar(usSettlements, arabSettlements);
+
+        final LocalDate thursday = LocalDate.of(2017, 9, 21);
+        final LocalDate friday = thursday.plusDays(1);
+        final LocalDate saturday = friday.plusDays(1);
+        final LocalDate monday = saturday.plusDays(2);
+        final LocalDate tuesday = monday.plusDays(1);
+
+        when(usSettlements.next(thursday)).thenReturn(new SettlementDate(monday));
+        when(usSettlements.next(friday)).thenReturn(new SettlementDate(tuesday));
+        when(usSettlements.next(saturday)).thenReturn(new SettlementDate(tuesday));
+
+        when(arabSettlements.contains(thursday)).thenReturn(true);
+        when(arabSettlements.contains(monday)).thenReturn(true);
+        when(arabSettlements.contains(tuesday)).thenReturn(true);
+
+        //Then
+        assertThat(pair.next(thursday).date(), is(monday));
+        assertThat(pair.next(friday).date(), is(tuesday));
+        assertThat(pair.next(saturday).date(), is(tuesday));
+
     }
 
 }
