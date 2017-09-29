11327fddfd1fd888b8e27764477c32e79717a0f3
Ollie Robertshaw <ollie.robertshaw@gmail.com>
2017-09-29 22:34:17 +0100

    MICs

--- schema/external/src/main/java/net/meerkat/identifier/market/Mic.java
+++ schema/external/src/main/java/net/meerkat/identifier/market/Mic.java
@@ -8,10 +8,15 @@ import net.meerkat.identifier.Iso;
  *
  * @author ollie
  * @see <a href="https://en.wikipedia.org/wiki/Market_Identifier_Code">MIC</a>
+ * @see <a href="http://www.iotafinance.com/en/ISO-10383-Market-Identification-Codes-MIC.html">MICs</a>
  */
 public class Mic
         extends StringWrapper
         implements Iso, MarketId {
+
+    public static Mic NASDAQ = new Mic("XNAS");
+    public static Mic LSE = new Mic("XLON");
+    public static Mic BOERSE = new Mic("XBER");
 
     public Mic(final String value) {
         super(value);
