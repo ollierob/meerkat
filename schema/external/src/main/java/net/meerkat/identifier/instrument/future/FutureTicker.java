1c5363c928ac319f4090c1bcc7d4fddab593aee3
Ollie Robertshaw <ollie.robertshaw@gmail.com>
2017-09-29 21:56:41 +0100

    Future ticker

--- schema/external/src/main/java/net/meerkat/identifier/instrument/future/FutureTicker.java
+++ schema/external/src/main/java/net/meerkat/identifier/instrument/future/FutureTicker.java
@@ -10,16 +10,10 @@ public class FutureTicker implements InstrumentId {
 
     private final FutureUnderlyingId underlying;
     private final FutureDelivery delivery;
-    private final String suffix;
 
     public FutureTicker(final FutureUnderlyingId underlying, final FutureDelivery delivery) {
-        this(underlying, delivery, null);
-    }
-
-    public FutureTicker(final FutureUnderlyingId underlying, final FutureDelivery delivery, final String suffix) {
         this.underlying = underlying;
         this.delivery = delivery;
-        this.suffix = suffix;
     }
 
     public FutureUnderlyingId underlying() {
@@ -30,14 +24,9 @@ public class FutureTicker implements InstrumentId {
         return delivery;
     }
 
-    public String suffix() {
-        return suffix;
-    }
-
     public String toTickerString() {
-        return underlying
-                + delivery.toDeliveryString()
-                + (suffix == null ? "" : suffix);
+        return underlying.name()
+                + delivery.toDeliveryString();
     }
 
 }
