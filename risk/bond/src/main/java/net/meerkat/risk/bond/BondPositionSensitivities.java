6d8ba1f9f6feeab7fbf9113ef214c3c912b2c211
Ollie <ollie.robertshaw@gmail.com>
2017-09-30 14:42:22 +0100

    Sensitivities

--- risk/bond/src/main/java/net/meerkat/risk/bond/BondPositionSensitivities.java
+++ risk/bond/src/main/java/net/meerkat/risk/bond/BondPositionSensitivities.java
@@ -1,6 +1,9 @@
 package net.meerkat.risk.bond;
 
 import java.util.Map;
+import java.util.Objects;
+
+import javax.annotation.Nonnull;
 
 import net.meerkat.calculate.sensitivity.yield.DollarDuration;
 import net.meerkat.risk.sensitivities.PositionSensitivities;
@@ -10,14 +13,15 @@ import net.meerkat.sensitivity.bond.BondSensitivities;
 /**
  *
  * @author Ollie
+ * @see BondPosition
  */
 public class BondPositionSensitivities implements BondSensitivities, PositionSensitivities {
 
     private final BondInstrumentSensitivities unit;
     private final long quantity;
 
-    protected BondPositionSensitivities(final BondInstrumentSensitivities unit, final long quantity) {
-        this.unit = unit;
+    public BondPositionSensitivities(@Nonnull final BondInstrumentSensitivities unit, final long quantity) {
+        this.unit = Objects.requireNonNull(unit, "sensitivities");
         this.quantity = quantity;
     }
 
