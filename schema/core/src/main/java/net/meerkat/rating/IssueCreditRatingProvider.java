275f018fbc8401a96f2f66ff1a80b55b9c4cf6a2
Ollie Robertshaw <ollie.robertshaw@gmail.com>
2017-10-03 21:07:19 +0100

    Credit rating

--- schema/core/src/main/java/net/meerkat/rating/IssueCreditRatingProvider.java
+++ schema/core/src/main/java/net/meerkat/rating/IssueCreditRatingProvider.java
@@ -1,5 +1,9 @@
 package net.meerkat.rating;
 
+import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
+
+import net.meerkat.issue.HasIssueId;
 import net.meerkat.issue.IssueId;
 
 /**
@@ -7,5 +11,15 @@ import net.meerkat.issue.IssueId;
  * @author ollie
  */
 public interface IssueCreditRatingProvider extends CreditRatingProvider<IssueId> {
+
+    @CheckForNull
+    default CreditRating get(final HasIssueId hasIssueId) {
+        return this.get(hasIssueId.issueId());
+    }
+
+    @Nonnull
+    default CreditRating require(final HasIssueId hasIssueId) throws UnavailableCreditRatingException {
+        return this.require(hasIssueId.issueId());
+    }
 
 }
--- schema/core/src/main/java/net/meerkat/rating/IssuerCreditRatingProvider.java
+++ schema/core/src/main/java/net/meerkat/rating/IssuerCreditRatingProvider.java
@@ -1,5 +1,9 @@
 package net.meerkat.rating;
 
+import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
+
+import net.meerkat.issuer.HasIssuerId;
 import net.meerkat.issuer.IssuerId;
 
 /**
@@ -7,5 +11,15 @@ import net.meerkat.issuer.IssuerId;
  * @author ollie
  */
 public interface IssuerCreditRatingProvider extends CreditRatingProvider<IssuerId> {
+
+    @CheckForNull
+    default CreditRating get(final HasIssuerId hasIssuerId) {
+        return this.get(hasIssuerId.issuerId());
+    }
+
+    @Nonnull
+    default CreditRating require(final HasIssuerId hasIssuerId) throws UnavailableCreditRatingException {
+        return this.require(hasIssuerId.issuerId());
+    }
 
 }
