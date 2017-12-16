dd0122c51112da371e829c348d70c57badab8c64
Ollie Robertshaw <ollie.robertshaw@gmail.com>
2017-12-16 13:30:28 +0000

    Dividends

--- schema/equity/src/main/java/net/meerkat/instrument/equity/dividend/Dividends.java
+++ schema/equity/src/main/java/net/meerkat/instrument/equity/dividend/Dividends.java
@@ -4,6 +4,13 @@ import net.meerkat.identifier.currency.CurrencyId;
 import net.meerkat.identifier.currency.HasCurrencyId;
 import net.meerkat.time.calendar.TemporalSequence;
 
+import javax.annotation.Nonnull;
+import java.util.function.Predicate;
+
 public interface Dividends<C extends CurrencyId> extends HasCurrencyId, TemporalSequence<Dividend<C>> {
 
+    @Nonnull
+    @Override
+    Dividends<C> filter(Predicate<? super Dividend<C>> predicate);
+    
 }
